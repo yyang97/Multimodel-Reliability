d21d3e567bb95190fc7995c43aa2028038273d3c
yyang97 <y789yang@uwaterloo.ca>
2021-03-10 16:43:17 +0800

    add sh

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+./CanADM_snow_Queb Can_theta.csv 0.7
+./CanADM_snow_Queb Can_theta.csv 0.8
+./CanADM_snow_Queb Can_theta.csv 0.9
+./CanADM_snow_Queb Can_theta.csv 1.0
+./CanADM_snow_Queb Can_theta.csv 1.1
+./CanADM_snow_Queb Can_theta.csv 1.2
+./CanADM_snow_Queb Can_theta.csv 1.3
+./USADM_snow_Queb Can_theta.csv 0.7
+./USADM_snow_Queb Can_theta.csv 0.8
+./USADM_snow_Queb Can_theta.csv 0.9
+./USADM_snow_Queb Can_theta.csv 1.0
+./USADM_snow_Queb Can_theta.csv 1.1
+./USADM_snow_Queb Can_theta.csv 1.2
+./USADM_snow_Queb Can_theta.csv 1.3
+./CanADM_snow_Van Can_theta.csv 0.7
+./CanADM_snow_Van Can_theta.csv 0.8
+./CanADM_snow_Van Can_theta.csv 0.9
+./CanADM_snow_Van Can_theta.csv 1.0
+./CanADM_snow_Van Can_theta.csv 1.1
+./CanADM_snow_Van Can_theta.csv 1.2
+./CanADM_snow_Van Can_theta.csv 1.3
+./USADM_snow_Van Can_theta.csv 0.7
+./USADM_snow_Van Can_theta.csv 0.8
+./USADM_snow_Van Can_theta.csv 0.9
+./USADM_snow_Van Can_theta.csv 1.0
+./USADM_snow_Van Can_theta.csv 1.1
+./USADM_snow_Van Can_theta.csv 1.2
+./USADM_snow_Van Can_theta.csv 1.3
+
+
+
+
+
